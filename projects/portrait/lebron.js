0fea6e2098428572199f141de414c7408b010b70
Tymur Bondariev <theoneandonlyt11m@gmail.com>
2026-02-16 18:04:43 -0500

    fixed nose, added ears, added teeth, still need hair. Aidan defs used Ai on his project

--- projects/portrait/lebron.js
+++ projects/portrait/lebron.js
@@ -60,6 +60,63 @@ ctx.beginPath();
 ctx.moveTo(475, 350);
 ctx.lineTo(450, 450);
 ctx.lineTo(500, 450);
-ctx.fillStyle = "black";
+ctx.outlineWidth = 5;
+ctx.strokeStyle = "black";
+ctx.lineWidth = 5;
+ctx.stroke();
+ctx.closePath();
+
+// Draw the mouth
+ctx.beginPath();
+ctx.arc(475, 600, 120, 0, Math.PI * 1);
+ctx.fillStyle = "red";
 ctx.fill();
 ctx.closePath();
+
+// Draw the eyebrows
+ctx.beginPath();
+ctx.moveTo(300, 200);
+ctx.lineTo(400, 250);
+ctx.lineWidth = 15;
+ctx.strokeStyle = "black";
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(550, 250);
+ctx.lineTo(650, 200);
+ctx.lineWidth = 15;
+ctx.strokeStyle = "black";
+ctx.stroke();
+ctx.closePath();
+
+// Draw the ears
+ctx.fillStyle = "tan";
+ctx.beginPath();
+ctx.ellipse(55,375,50,110,0,0,Math.PI * 2);
+ctx.fill();
+
+ctx.fillStyle = "tan";
+ctx.beginPath();
+ctx.ellipse(895,375,50,110,0,0,Math.PI * 2);
+ctx.fill();
+
+// Draw the teeth
+//1st tooth
+ctx.fillStyle = "white";
+ctx.fillRect(380, 600, 30, 20);
+//2nd tooth
+ctx.fillStyle = "white";
+ctx.fillRect(420, 600, 30, 20);
+//3rd tooth
+ctx.fillStyle = "white";
+ctx.fillRect(460, 600, 30, 20);
+//4th tooth
+ctx.fillStyle = "white";
+ctx.fillRect(500, 600, 30, 20);
+//5th tooth
+ctx.fillStyle = "white";
+ctx.fillRect(540, 600, 30, 20); 
+
+
+

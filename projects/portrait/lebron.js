88aa2872923ae65b64b4524a1d4cd7f9ff91d91e
Tymur Bondariev <theoneandonlyt11m@gmail.com>
2026-02-16 20:25:49 -0500

    did the hair, dont llike it tho

--- projects/portrait/lebron.js
+++ projects/portrait/lebron.js
@@ -118,5 +118,189 @@ ctx.fillRect(500, 600, 30, 20);
 ctx.fillStyle = "white";
 ctx.fillRect(540, 600, 30, 20); 
 
+// Draw the hair
+
+ctx.beginPath();
+ctx.moveTo(190, 110);
+ctx.lineTo(190, 250);
+ctx.lineWidth = 20;
+ctx.strokeStyle = "rgb(101, 67, 33)";
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(220, 90);
+ctx.lineTo(220, 260);
+ctx.lineWidth = 20;
+ctx.strokeStyle = "rgb(101, 67, 33)";
+ctx.stroke();
+ctx.closePath(); 
+
+
+ctx.fillStyle = "rgb(101, 67, 33)";
+ctx.beginPath();
+ctx.moveTo(250, 60);
+ctx.lineTo(250, 230);
+ctx.lineWidth = 20;
+ctx.strokeStyle = "rgb(101, 67, 33)";
+ctx.stroke();
+ctx.closePath();
+
+ctx.fillStyle = "rgb(101, 67, 33)";
+ctx.beginPath();
+ctx.moveTo(280, 40);
+ctx.lineTo(280, 250);
+ctx.lineWidth = 20;
+ctx.strokeStyle = "rgb(101, 67, 33)";
+ctx.stroke();
+ctx.closePath();
+
+ctx.fillStyle = "rgb(101, 67, 33)";
+ctx.beginPath();
+ctx.moveTo(310, 30);
+ctx.lineTo(310, 190);
+ctx.lineWidth = 20;
+ctx.strokeStyle = "rgb(101, 67, 33)";
+ctx.stroke();
+ctx.closePath();
+
+ctx.fillStyle = "rgb(101, 67, 33)";
+ctx.beginPath();
+ctx.moveTo(340, 20);
+ctx.lineTo(340, 200);
+ctx.lineWidth = 20;
+ctx.strokeStyle = "rgb(101, 67, 33)";
+ctx.stroke();
+ctx.closePath();
+
+ctx.fillStyle = "rgb(101, 67, 33)";
+ctx.beginPath();
+ctx.moveTo(370, 10);
+ctx.lineTo(370, 220);
+ctx.lineWidth = 20;
+ctx.strokeStyle = "rgb(101, 67, 33)";
+ctx.stroke();
+ctx.closePath();
+
+ctx.fillStyle = "rgb(101, 67, 33)";
+ctx.beginPath();
+ctx.moveTo(400, 5);
+ctx.lineTo(400, 210);
+ctx.lineWidth = 20;
+ctx.strokeStyle = "rgb(101, 67, 33)";
+ctx.stroke();
+ctx.closePath();
+
+ctx.fillStyle = "rgb(101, 67, 33)";
+ctx.beginPath();
+ctx.moveTo(430, 0);
+ctx.lineTo(430, 230);
+ctx.lineWidth = 20;
+ctx.strokeStyle = "rgb(101, 67, 33)";
+ctx.stroke();
+ctx.closePath();
+
+ctx.fillStyle = "rgb(101, 67, 33)";
+ctx.beginPath();
+ctx.moveTo(460, 0);
+ctx.lineTo(460, 240);
+ctx.lineWidth = 20;
+ctx.strokeStyle = "rgb(101, 67, 33)";
+ctx.stroke();
+ctx.closePath();
+
+ctx.fillStyle = "rgb(101, 67, 33)";
+ctx.beginPath();
+ctx.moveTo(490, 0);
+ctx.lineTo(490, 220);
+ctx.lineWidth = 20;
+ctx.strokeStyle = "rgb(101, 67, 33)";
+ctx.stroke();
+ctx.closePath();
+
+ctx.fillStyle = "rgb(101, 67, 33)";
+ctx.beginPath();
+ctx.moveTo(520, 3);
+ctx.lineTo(520, 230);
+ctx.lineWidth = 20;
+ctx.strokeStyle = "rgb(101, 67, 33)";
+ctx.stroke();
+ctx.closePath();
+
+
+ctx.beginPath();
+ctx.moveTo(550, 5);
+ctx.lineTo(550, 250);
+ctx.lineWidth = 20;
+ctx.strokeStyle = "rgb(101, 67, 33)";
+ctx.stroke();
+ctx.closePath();
+
+
+ctx.beginPath();
+ctx.moveTo(580, 10);
+ctx.lineTo(580, 220);
+ctx.lineWidth = 20;
+ctx.strokeStyle = "rgb(101, 67, 33)";
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(610, 20);
+ctx.lineTo(610, 200);
+ctx.lineWidth = 20;
+ctx.strokeStyle = "rgb(101, 67, 33)";
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(640, 30);
+ctx.lineTo(640, 190);
+ctx.lineWidth = 20;
+ctx.strokeStyle = "rgb(101, 67, 33)";
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(670, 40);
+ctx.lineTo(670, 200);
+ctx.lineWidth = 20;
+ctx.strokeStyle = "rgb(101, 67, 33)";
+ctx.stroke();
+ctx.closePath();
+
+
+ctx.beginPath();
+ctx.moveTo(700, 70);
+ctx.lineTo(700, 220);
+ctx.lineWidth = 20;
+ctx.strokeStyle = "rgb(101, 67, 33)";
+ctx.stroke();
+ctx.closePath();
+
+
+ctx.beginPath();
+ctx.moveTo(730, 90);
+ctx.lineTo(730, 250);
+ctx.lineWidth = 20;
+ctx.strokeStyle = "rgb(101, 67, 33)";
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(760, 120);
+ctx.lineTo(760, 240);
+ctx.lineWidth = 20;
+ctx.strokeStyle = "rgb(101, 67, 33)";
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(790, 150);
+ctx.lineTo(790, 250);
+ctx.lineWidth = 20;
+ctx.strokeStyle = "rgb(101, 67, 33)";
+ctx.stroke();
+ctx.closePath();
 
 

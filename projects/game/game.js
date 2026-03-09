33258e01b391319c5098f2b224123579db57c55d
Tymur Bondariev <theoneandonlyt11m@gmail.com>
2026-03-09 09:22:12 -0400

    Added first locations, functions, and some of the moving.

--- projects/game/game.js
+++ projects/game/game.js
@@ -2,32 +2,66 @@ let gameActive = true; //this variable is required.
                        //to stop the game, set it to false.
 
 //Declare your other global variables here
+let currentLocation;
+let inventory = [];
+let hasKey = false;
+let hasPassword = false;
+let energy = 0;
+
 
 
 //If you need, add any "helper" functions here
 
 
 //Make one function for each location
-function locationA() {
+function Commons() {
     clear();
-    print("\nYou are in location A!");
+    print("\nYou are in commons");
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlocationB");
+        "\n\tGo to the table and talk to Prithvi" +
+        "\n\tMain Hallway");
     
     function processInput(input){
-        if (input.toLowerCase() === "locationb") {
-            locationB();
-        } else {
+        if (input.toLowerCase() === "go to the table and talk to prithvi") {
+            Prithvi();
+        } else if (input.toLowerCase() === "main hallway") {
+            mainHallway();
+    } else {
             stayHere();
-            waitThenCall(locationA);
+            waitThenCall(Commons);
         }
     }
     waitForInput(processInput);
 }
 
-function locationB() {
+function Prithvi() {
     clear();
-    print("\nYou are in location B!");
+    print("\nYou are talking to Prithvi!");
+    print("\nWhat do you want to do? Say one of these choices:" +
+        "\n\tAsk about the key" +
+        "\n\tGet an energy drink" +
+        "\n\tGo back to commons");
+
+    function processInput(input){
+        if (input.toLowerCase() === "ask about the key") {
+            if (hasKey) {
+                print("\nPrithvi says: Ehh, I don't know anything about it... maybe search the building?");
+            } 
+        } else if (input.toLowerCase() === "get an energy drink") {
+            energy += 10;
+            print("\nYou feel more energetic!");
+        } else if (input.toLowerCase() === "go back to commons") {
+            Commons();
+        }
+    }
+    waitForInput(processInput);
+}
+               
+    waitForInput(processInput);
+
+function mainHallway() {
+    clear();
+    print("\nYou are in the main hallway!");
     print("\nWhere do you want to go next? Say one of these choices:" +
         "\n\tlocationA");
     
@@ -49,7 +83,7 @@ function start(){
     print("Welcome to my game! Press any key to start");
 
     function processInput(input){
-            locationA();
+            Commons();
     }
     waitForInput(processInput);
 }

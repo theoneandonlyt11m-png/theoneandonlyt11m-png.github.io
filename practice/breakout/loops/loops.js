25635dbbd9b6f4065df4ca97c3fae175ed552d2b
Tymur Bondariev <theoneandonlyt11m@gmail.com>
2026-03-26 09:24:40 -0400

    classwork

---
+++ practice/breakout/loops/loops.js
@@ -0,0 +1,29 @@
+let canvas = document.getElementById("canvas1");
+let ctx = canvas.getContext("2d");
+
+//fill in code for canvas1 here
+for (let x = 0; x < canvas.width; x += 75) {
+    for (let y = 0; y <= 150; y += 75) {
+        ctx.fillRect(x, y, 50, 20);
+    }
+}
+
+canvas = document.getElementById("canvas2");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas2 here
+for (let x = 0; x < y; x += 100) {
+    for (let y = 50; y <= 800; y += 100) {
+        ctx.fillRect(x, y, 80, 25);
+    }
+}
+
+canvas = document.getElementById("canvas3");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas3 here
+
+canvas = document.getElementById("canvas4");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas4 here

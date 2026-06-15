2584c4b92b9e18ff797d3987184e31625061324a
Tim Bond <theoneandolnyt11m@gmail.com>
2026-06-14 21:44:11 -0700

    added a main page with buttons

--- js/validate.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  var script = document.createElement("script");
-  script.src = "https://cdn.jsdelivr.net/gh/gracehoppercenter/validate@1.0.5/validate.js";
-  script.async = false;
-  document.head.appendChild(script);
-})();
---
+++ script.js
@@ -0,0 +1,7 @@
+function ViewProjects() {
+    
+}
+
+function ViewAbout() {
+    alert("About Me page is under construction. Please check back later!");
+}
